0d9e19e5364d7d0681fdb7f8b090302793e5331e
thrombe <yash.n.thombare620@gmail.com>
2025-11-03 02:54:23 +0530

    add a pkg-publish command

--- extension/src/dev.ts
+++ extension/src/dev.ts
@@ -59,7 +59,7 @@ async function main() {
 
         cliOpts.filename = `${name}-v${version}.zip`;
         await webExt.cmd.build(cliOpts, { shouldExitProgram: true });
-    } else if (command === "sign") {
+    } else if (command === "sign" || command === "publish") {
         const manifest = await get_manifest();
         const name = manifest["name"];
         const version = manifest["version"];
@@ -67,24 +67,18 @@ async function main() {
         const src_zip = `./build_artifacts/${name}-src-v${version}.zip`;
         await promisify(exec)(`git -C "$(git rev-parse --show-toplevel)" archive --format=zip -o ./extension/${src_zip} HEAD`);
 
-        const api_key = env["FF_API_USER"];
-        const api_secret = env["FF_API_KEY"];
-        const artifacts_dir = './build_artifacts';
-        const amo_meta = './amo-meta.json';
-        const source_dir = './build';
-        await promisify(exec)(`bun x web-ext sign --api-key="${api_key}" --api-secret="${api_secret}" --channel=unlisted --amo-metadata="${amo_meta}" --source-dir="${source_dir}" --artifacts-dir="${artifacts_dir}" --upload-source-code="${src_zip}"`);
-
-        // TODO: throws error
-        // await webExt.cmd.sign({
-        //     artifactsDir: './build_artifacts',
-        //     amoBaseUrl: 'https://addons.mozilla.org',
-        //     apiKey: env["FF_API_USER"],
-        //     apiSecret: env["FF_API_KEY"],
-        //     amoMetadata: './amo-meta.json',
-        //     sourceDir: './build',
-        //     uploadSourceCode: src_zip,
-        //     channel: "unlisted",
-        // });
+        // - [web-ext command reference | Firefox Extension Workshop](https://extensionworkshop.com/documentation/develop/web-ext-command-reference/#web-ext-sign)
+        await webExt.cmd.sign({
+            artifactsDir: './build_artifacts',
+            amoBaseUrl: 'https://addons.mozilla.org/api/v5/',
+            apiKey: env["FF_API_USER"],
+            apiSecret: env["FF_API_KEY"],
+            amoMetadata: './amo-meta.json',
+            sourceDir: './build',
+            uploadSourceCode: src_zip,
+            channel: command === "publish" ? "listed" : "unlisted",
+            approvalCheckTimeout: command === "publish" ? 0 : undefined,
+        });
     }
 }
 
